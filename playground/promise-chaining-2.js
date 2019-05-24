08eb2d2368b564bf1f758a84935064232587179d
Sandesh Tiwari <sandeshtiwari@live.com>
2019-05-23 23:43:01 -0500

    feat: setup routers

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -2,11 +2,24 @@ require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
 
-Task.findByIdAndDelete('5ce4a2ecab15253458fbd910').then((task) => {
-  console.log(task);
-  return Task.countDocuments({ completed: false })
-}).then((result) => {
-  console.log(result);
+// Task.findByIdAndDelete('5ce376c8bdc1f418180c8c06').then((task) => {
+//   console.log(task);
+//   return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//   console.log(result);
+// }).catch((e) => {
+//   console.log(e);
+// })
+
+
+const deleteTaskAndCount = async (id) => {
+  const task = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+}
+
+deleteTaskAndCount('5ce4a288d58d412524c177e1').then((count) => {
+  console.log(count)
 }).catch((e) => {
-  console.log(e);
+  console.log(e)
 })
